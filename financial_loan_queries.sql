5a6b476d95578a8a6fe6e7ab6ad2f9bac4438021
oumaimadaham <oumaimadaham@outlook.com>
2025-08-16 14:44:40 +0300

    Update financial_loan_queries.sql

--- financial_loan_queries.sql
+++ financial_loan_queries.sql
@@ -3,3 +3,18 @@
 SELECT COUNT(ID) AS Total_Loan_Applications
 FROM Loan;
 
+-- Total Loan Amount
+SELECT SUM(loan_amount) AS Total_Loan_Amount 
+FROM Loan;
+
+-- Total Loan Amount Month-to-Date (MTD)
+SELECT SUM(loan_amount) AS MTD_Total_Loan_Amount
+FROM Loan
+WHERE MONTH(issue_date) = 12
+  AND YEAR(issue_date) = 2021;
+
+
+
+
+
+
