9f9e30ab969d00801fd0605a36d5ed2980458bad
oumaimadaham <oumaimadaham@outlook.com>
2025-08-18 12:59:10 +0300

    Update bank_loan_queries.sql

--- bank_loan_queries.sql
+++ bank_loan_queries.sql
@@ -3,18 +3,18 @@
 SELECT COUNT(ID) AS Total_Loan_Applications
 FROM Loan;
 
--- Total Loan Amount
+-- Total Funded Amount
 SELECT SUM(loan_amount) AS Total_Loan_Amount 
 FROM Loan;
 
--- Total Loan Amount Month-to-Date (MTD)
-SELECT SUM(loan_amount) AS MTD_Total_Loan_Amount
+-- Total Funded Amount Month-to-Date (MTD)
+SELECT SUM(loan_amount) AS MTD_Total_Funded_Amount
 FROM Loan
 WHERE MONTH(issue_date) = 12
   AND YEAR(issue_date) = 2021;
 
--- Total Loan Amount Previous Month (PMTD)
-SELECT SUM(loan_amount) AS PMTD_Total_Loan_Amount
+-- Total Funded Amount Previous Month (PMTD)
+SELECT SUM(loan_amount) AS PMTD_Total_Funded_Amount
 FROM Loan
 WHERE MONTH(issue_date) = 11
   AND YEAR(issue_date) = 2021;
